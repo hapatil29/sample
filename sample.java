86bc39941e277e8260fdc586ea3168d44f8b0198
hapatil29 <hapatil29@gmail.com>
2019-03-26 13:25:31 +0530

    code remodified

--- sample.java
+++ sample.java
@@ -2,7 +2,7 @@ public class sample
 {
   public static void main (String[] args){
   
-      System.out.println("Java Code under construction");
+      System.out.println("java recoded");
   
   }
 }
